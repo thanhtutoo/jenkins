8eb51eac7738ee8596ee086b6db3987afc5aa742
fuegoai <thanhtuto@parcelsanta.com>
2020-04-11 21:03:44 +0800

    aa

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res) {
-  res.send("Hello Worlds!! Test Okssssssssdsss");
+  res.send("Hello Worlds!! Test");
 });
 
 app.listen(3000);
